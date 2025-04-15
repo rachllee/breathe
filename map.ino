f552cebe249e366478ba6ca933c498e419a72b2e
Rachel Lee <134354574+rachllee@users.noreply.github.com>
2025-04-15 17:18:48 -0400

    Update map.ino

--- map.ino
+++ map.ino
@@ -1,14 +1,19 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "Rachels iPhone";
+const char* password = "roachhhh";
+
 const int dirPin = 14;
 const int stepPin = 32;
 
-int direction = 1; // 1 is clockwise, vice versa
+int direction = 1;
 int latestAQI = 0;
 
 unsigned long lastStepTime = 0;
-unsigned long stepDelay = 1000 / 200;
-unsigned long cycleStartTime = 0;
-unsigned long cycleDuration = 3000; 
-bool hiccupActive = false;
+float stepDelay = 8;
+const int stepsPerCycle = 1000;
+int currentStep = 0;
 
 WebServer server(80);
 
@@ -17,12 +22,17 @@ void handleAQIPost() {
     latestAQI = server.arg("aqi").toInt();
     Serial.print("Received AQI: ");
     Serial.println(latestAQI);
+
+
+    stepDelay = 15.0 * pow(0.985, latestAQI);
+    Serial.print("Updated step delay (ms): ");
+    Serial.println(stepDelay);
+
     server.send(200, "text/plain", "AQI received!");
   } else {
     server.send(400, "text/plain", "Missing 'aqi' parameter.");
   }
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -36,6 +46,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("\nWiFi connected.");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -43,48 +54,26 @@ void setup() {
   server.on("/aqi", HTTP_POST, handleAQIPost);
   server.begin();
   Serial.println("Server started.");
-
-  cycleStartTime = millis();
 }
 
 void loop() {
   server.handleClient();
+
   unsigned long now = millis();
-
-  if (now - cycleStartTime >= cycleDuration) {
-    direction = !direction;
-    digitalWrite(dirPin, direction);
-    cycleStartTime = now;
-  }
-
-  if (shouldHiccup(latestAQI)) {
-    performHiccup();
-    return;
-  }
 
   if (now - lastStepTime >= stepDelay) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(100);
     digitalWrite(stepPin, LOW);
     lastStepTime = now;
+
+    currentStep++;
+
+    if (currentStep >= stepsPerCycle) {
+      direction = !direction;
+      digitalWrite(dirPin, direction);
+      currentStep = 0;
+    }
   }
 }
 
-void performHiccup() {
-  int hiccupSteps = random(1, 3);
-  digitalWrite(dirPin, !direction);
-
-  for (int i = 0; i < hiccupSteps; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(100);
-    digitalWrite(stepPin, LOW);
-    delay(stepDelay);
-  }
-
-  digitalWrite(dirPin, direction);
-}
-
-bool shouldHiccup(int aqi) {
-  int hiccupChance = map(constrain(aqi, 0, 300), 0, 300, 0, 70);
-  return random(0, 100) < hiccupChance;
-}
